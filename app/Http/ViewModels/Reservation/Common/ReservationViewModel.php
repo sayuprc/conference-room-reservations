3ab11388a2ddcb9b8beba4c59597443775cff0b4
sayuprc <mkpoto15@gmail.com>
2022-07-16 08:58:42 +0900

    fix: Formatting modifications.

--- app/Http/ViewModels/Reservation/Common/ReservationViewModel.php
+++ app/Http/ViewModels/Reservation/Common/ReservationViewModel.php
@@ -55,8 +55,7 @@ class ReservationViewModel
         DateTimeInterface $startAt,
         DateTimeInterface $endAt,
         string $note
-    )
-    {
+    ) {
         $this->roomId = $roomId;
         $this->reservationId = $reservationId;
         $this->summary = $summary;
