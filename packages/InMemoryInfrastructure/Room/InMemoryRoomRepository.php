0f3274d9c5cc76c06c16e8cdcf5cf317784e9ee7
sayuprc <mkpoto15@gmail.com>
2022-07-24 10:22:46 +0900

    refactor: Split repository storage methods by purpose. #51

--- packages/InMemoryInfrastructure/Room/InMemoryRoomRepository.php
+++ packages/InMemoryInfrastructure/Room/InMemoryRoomRepository.php
@@ -4,13 +4,6 @@ declare(strict_types=1);
 
 namespace packages\InMemoryInfrastructure\Room;
 
-use DateTime;
-use packages\Domain\Domain\Reservation\EndAt;
-use packages\Domain\Domain\Reservation\Note;
-use packages\Domain\Domain\Reservation\Reservation;
-use packages\Domain\Domain\Reservation\ReservationId;
-use packages\Domain\Domain\Reservation\StartAt;
-use packages\Domain\Domain\Reservation\Summary;
 use packages\Domain\Domain\Room\Exception\NotFoundException;
 use packages\Domain\Domain\Room\Room;
 use packages\Domain\Domain\Room\RoomId;
@@ -32,21 +25,7 @@ class InMemoryRoomRepository implements RoomRepositoryInterface
         foreach (range(1, 20) as $i) {
             $roomId = new RoomId((string)$i);
 
-            $reservations = [];
-
-            foreach (range(1, 30) as $j) {
-                $reservationsId = new ReservationId((string)$j);
-                $reservations[$reservationsId->getValue()] = new Reservation(
-                    $roomId,
-                    $reservationsId,
-                    new Summary('概要 ' . $j),
-                    new StartAt(new DateTime()),
-                    new EndAt((new DateTime())->modify('+' . $j . ' hours')),
-                    new Note('備考 ' . $j)
-                );
-            }
-
-            $this->db[$roomId->getValue()] = new Room($roomId, new RoomName('会議室 ' . $i), $reservations);
+            $this->db[$roomId->getValue()] = new Room($roomId, new RoomName('会議室 ' . $i));
         }
     }
 
@@ -81,16 +60,38 @@ class InMemoryRoomRepository implements RoomRepositoryInterface
     }
 
     /**
-     * 会議室の保存を行う。
+     * 会議室の新規保存を行う。
      *
      * @param Room $room
      *
      * @return void
      */
-    public function store(Room $room): void
+    public function insert(Room $room): void
     {
-        $storedRoom = $this->db[$room->getRoomId()->getValue()] ?? $room;
+        $this->db[$room->getRoomId()->getValue()] = $room;
+    }
 
-        $this->db[$storedRoom->getRoomId()->getValue()] = $room;
+    /**
+     * 会議室の更新を行う。
+     *
+     * @param Room $room
+     *
+     * @return void
+     */
+    public function update(Room $room): void
+    {
+        $this->db[$room->getRoomId()->getValue()] = $room;
+    }
+
+    /**
+     * 会議室の削除を行う。
+     *
+     * @param RoomId $roomId
+     *
+     * @return void
+     */
+    public function delete(RoomId $roomId): void
+    {
+        unset($this->db[$roomId->getValue()]);
     }
 }
