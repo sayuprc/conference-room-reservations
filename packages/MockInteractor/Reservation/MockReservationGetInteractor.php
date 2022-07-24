1ec0ce134c57512df5896371ec179795e4075f94
sayuprc <mkpoto15@gmail.com>
2022-07-24 09:52:45 +0900

    refactor: Implementing methods for mocking.

--- packages/MockInteractor/Reservation/MockReservationGetInteractor.php
+++ packages/MockInteractor/Reservation/MockReservationGetInteractor.php
@@ -11,9 +11,7 @@ use packages\Domain\Domain\Reservation\Reservation;
 use packages\Domain\Domain\Reservation\ReservationId;
 use packages\Domain\Domain\Reservation\StartAt;
 use packages\Domain\Domain\Reservation\Summary;
-use packages\Domain\Domain\Room\Exception\NotFoundException;
 use packages\Domain\Domain\Room\RoomId;
-use packages\Domain\Domain\Room\RoomRepositoryInterface;
 use packages\UseCase\Reservation\Get\ReservationGetRequest;
 use packages\UseCase\Reservation\Get\ReservationGetResponse;
 use packages\UseCase\Reservation\Get\ReservationGetUseCaseInterface;
@@ -21,26 +19,16 @@ use packages\UseCase\Reservation\Get\ReservationGetUseCaseInterface;
 class MockReservationGetInteractor implements ReservationGetUseCaseInterface
 {
     /**
-     * @var RoomRepositoryInterface $repository
-     */
-    private RoomRepositoryInterface $repository;
-
-    /**
-     * @param RoomRepositoryInterface $repository
-     *
      * @return void
      */
-    public function __construct(RoomRepositoryInterface $repository)
+    public function __construct()
     {
-        $this->repository = $repository;
     }
 
     /**
      * 予約の詳細を取得する。
      *
      * @param ReservationGetRequest $request
-     *
-     * @throws NotFoundException
      *
      * @return ReservationGetResponse
      */
@@ -49,30 +37,6 @@ class MockReservationGetInteractor implements ReservationGetUseCaseInterface
         $roomId = new RoomId($request->getRoomId());
         $reservationId = new ReservationId($request->getReservationId());
 
-        // // キーを振りなおすため、配列にマージする。
-        // $found = [...array_filter(
-        //     $this->repository->find($roomId)->getReservations(),
-        //     function (Reservation $reservation) use ($reservationId): bool {
-        //         return $reservation->getReservationId()->equals($reservationId);
-        //     }
-        // )];
-
-        // if (count($found) !== 1) {
-        //     throw new NotFoundException('ID: ' . $reservationId->getValue() . ' is not found.');
-        // }
-
-        // $reservation = new Reservation(
-        //     $found[0]->getRoomId(),
-        //     $found[0]->getReservationId(),
-        //     $found[0]->getSummary(),
-        //     $found[0]->getStartAt(),
-        //     $found[0]->getEndAt(),
-        //     $found[0]->getNote()
-        // );
-
-        // return new ReservationGetResponse($reservation);
-
-        // TODO 後で実装する。
         return new ReservationGetResponse(
             new Reservation(
                 $roomId,
--- packages/MockInteractor/Reservation/MockReservationUpdateInteractor.php
+++ packages/MockInteractor/Reservation/MockReservationUpdateInteractor.php
@@ -6,15 +6,12 @@ namespace packages\MockInteractor\Reservation;
 
 use DateTime;
 use packages\Domain\Domain\Reservation\EndAt;
-use packages\Domain\Domain\Reservation\Exception\PeriodicDuplicationException;
 use packages\Domain\Domain\Reservation\Note;
 use packages\Domain\Domain\Reservation\Reservation;
 use packages\Domain\Domain\Reservation\ReservationId;
-use packages\Domain\Domain\Reservation\ReservationService;
 use packages\Domain\Domain\Reservation\StartAt;
 use packages\Domain\Domain\Reservation\Summary;
 use packages\Domain\Domain\Room\RoomId;
-use packages\Domain\Domain\Room\RoomRepositoryInterface;
 use packages\UseCase\Reservation\Update\ReservationUpdateRequest;
 use packages\UseCase\Reservation\Update\ReservationUpdateResponse;
 use packages\UseCase\Reservation\Update\ReservationUpdateUseCaseInterface;
@@ -22,33 +19,16 @@ use packages\UseCase\Reservation\Update\ReservationUpdateUseCaseInterface;
 class MockReservationUpdateInteractor implements ReservationUpdateUseCaseInterface
 {
     /**
-     * @var RoomRepositoryInterface $repository
-     */
-    private RoomRepositoryInterface $repository;
-
-    /**
-     * @var ReservationService $service
-     */
-    private ReservationService $service;
-
-    /**
-     * @param RoomRepositoryInterface $repository
-     * @param ReservationService      $service
-     *
      * @return void
      */
-    public function __construct(RoomRepositoryInterface $repository, ReservationService $service)
+    public function __construct()
     {
-        $this->repository = $repository;
-        $this->service = $service;
     }
 
     /**
      * 予約の更新を行う。
      *
      * @param ReservationUpdateRequest $request
-     *
-     * @throws PeriodicDuplicationException
      *
      * @return ReservationUpdateResponse
      */
@@ -63,20 +43,6 @@ class MockReservationUpdateInteractor implements ReservationUpdateUseCaseInterfa
             new Note($request->getNote())
         );
 
-        // if (! $this->service->canUpdated($updatedReservation)) {
-        //     throw new PeriodicDuplicationException('There is a reservation for a specified period of time.');
-        // }
-
-        // $room = $this->repository->find($updatedReservation->getRoomId());
-
-        // // 一度対象の予約を削除してから再度会議室に追加する。
-        // $updatedRoom = $room
-        //     ->removeReservation($updatedReservation->getReservationId())
-        //     ->addReservation($updatedReservation);
-
-        // $this->repository->store($updatedRoom);
-
-        // TODO 後で実装する。
         return new ReservationUpdateResponse($updatedReservation);
     }
 }
