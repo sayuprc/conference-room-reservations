dfa7d5c1fcc357d2b5fc6375f172f10e59168de0
sayuprc <41261915+sayuprc@users.noreply.github.com>
2022-07-26 22:18:37 +0900

    style: Remove dark class. (#61)

--- resources/views/rooms/register.blade.php
+++ resources/views/rooms/register.blade.php
@@ -22,16 +22,16 @@
             </div>
         @endif
 
-        <section class="mx-auto mt-4 w-3/4 rounded-md bg-white p-6 shadow-md dark:bg-gray-800">
-            <h2 class="text-lg font-semibold capitalize text-gray-700 dark:text-white">会議室登録</h2>
+        <section class="mx-auto mt-4 w-3/4 rounded-md bg-white p-6 shadow-md">
+            <h2 class="text-lg font-semibold capitalize text-gray-700">会議室登録</h2>
 
             <form action="/rooms/register" method="POST">
                 @csrf
                 <div class="mt-4 grid grid-cols-1 gap-6 sm:grid-cols-2">
                     <div>
-                        <label class="text-gray-700 dark:text-gray-200" for="room_name">会議室名</label>
+                        <label class="text-gray-700" for="room_name">会議室名</label>
                         <input id="room_name" type="text" name="name"
-                            class="mt-2 block w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300">
+                            class="mt-2 block w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">
                     </div>
                 </div>
 
--- resources/views/rooms/reservations/detail.blade.php
+++ resources/views/rooms/reservations/detail.blade.php
@@ -54,9 +54,9 @@
             </div>
         @endif
 
-        <section class="mx-auto mt-4 w-3/4 rounded-md bg-white p-6 shadow-md dark:bg-gray-800">
+        <section class="mx-auto mt-4 w-3/4 rounded-md bg-white p-6 shadow-md">
             <div class="flex justify-between">
-                <h2 class="text-lg font-semibold capitalize text-gray-700 dark:text-white">予約詳細</h2>
+                <h2 class="text-lg font-semibold capitalize text-gray-700">予約詳細</h2>
 
                 <form action="/reservations/delete" method="post">
                     @csrf
@@ -73,41 +73,41 @@
                 <input type="hidden" name="reservation_id" value="{{ $reservation->reservationId }}">
                 <div class="mt-4">
                     <div>
-                        <label class="text-gray-700 dark:text-gray-200" for="summary">概要</label>
+                        <label class="text-gray-700" for="summary">概要</label>
                         <input
                             id="summary"
                             type="text"
                             name="summary"
                             value="{{ $reservation->summary }}"
-                            class="mt-2 block w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300">
+                            class="mt-2 block w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">
                     </div>
 
                     <div class="mt-2 flex items-center">
                         <div>
-                            <label class="block text-gray-700 dark:text-gray-200" for="start_at_date">開始日時</label>
+                            <label class="block text-gray-700" for="start_at_date">開始日時</label>
                             <input id="start_at_date" type="date" name="start_at_date"
                                 value="{{ $reservation->startAtDate }}"
-                                class="mt-2 rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300">
+                                class="mt-2 rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">
                             <input id="start_at_time" type="time" name="start_at_time"
                                 value="{{ $reservation->startAtTime }}"
-                                class="rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300">
+                                class="rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">
                         </div>
                         <span class="mx-4">~</span>
                         <div>
-                            <label class="block text-gray-700 dark:text-gray-200" for="end_at_date">終了日時</label>
+                            <label class="block text-gray-700" for="end_at_date">終了日時</label>
                             <input id="end_at_date" type="date" name="end_at_date"
                                 value="{{ $reservation->endAtDate }}"
-                                class="mt-2 rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300">
+                                class="mt-2 rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">
                             <input id="end_at_time" type="time" name="end_at_time"
                                 value="{{ $reservation->endAtTime }}"
-                                class="mt-2 rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300">
+                                class="mt-2 rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">
                         </div>
                     </div>
 
                     <div class="mt-2">
-                        <label class="text-gray-700 dark:text-gray-200" for="note">備考</label>
+                        <label class="text-gray-700" for="note">備考</label>
                         <textarea id="note" rows="4" name="note"
-                            class="mt-2 block w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300">{{ $reservation->note }}</textarea>
+                            class="mt-2 block w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">{{ $reservation->note }}</textarea>
                     </div>
                 </div>
 
