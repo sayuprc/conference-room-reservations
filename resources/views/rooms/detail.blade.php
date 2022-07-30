f643c46114843aaecff4de6365cfbc240523f86b
sayuprc <41261915+sayuprc@users.noreply.github.com>
2022-07-30 14:16:09 +0900

    feat: Adjust light mode. #63 (#64)

--- resources/views/rooms/detail.blade.php
+++ resources/views/rooms/detail.blade.php
@@ -3,72 +3,34 @@
 @section('title', '会議室詳細')
 
 @section('content')
-    @if (session('message'))
-        <div class="mx-auto my-4 w-3/4 bg-emerald-500 text-white">
-            <div class="container mx-auto flex items-center justify-between px-6 py-4">
-                <div class="flex">
-                    <svg viewBox="0 0 40 40" class="h-6 w-6 fill-current">
-                        <path
-                            d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM16.6667 28.3333L8.33337 20L10.6834 17.65L16.6667 23.6166L29.3167 10.9666L31.6667 13.3333L16.6667 28.3333Z">
-                        </path>
-                    </svg>
-
-                    <p class="mx-3">{{ session('message') }}</p>
-                </div>
-            </div>
-        </div>
-    @endif
-
-    @if (session('exception'))
-        <div class="mx-auto mt-4 w-3/4 bg-red-500 text-white">
-            <div class="container mx-auto flex items-center justify-start px-6 py-4">
-                <div class="flex">
-                    <svg viewBox="0 0 40 40" class="h-6 w-6 fill-current">
-                        <path
-                            d="M20 3.36667C10.8167 3.36667 3.3667 10.8167 3.3667 20C3.3667 29.1833 10.8167 36.6333 20 36.6333C29.1834 36.6333 36.6334 29.1833 36.6334 20C36.6334 10.8167 29.1834 3.36667 20 3.36667ZM19.1334 33.3333V22.9H13.3334L21.6667 6.66667V17.1H27.25L19.1334 33.3333Z">
-                        </path>
-                    </svg>
-                </div>
-                <div class="ml-1">{{ session('exception') }}</div>
-            </div>
-        </div>
-    @endif
-
-    <div class="mx-auto w-3/4">
-        <div class="flex">
-            <div class="bg-white">
-                <div>
-                    <p class="text-2xl font-bold text-gray-700"> {{ $room->name }}</p>
-                </div>
-            </div>
-
-            <div class="ml-6">
-                <form action="/reservations/register" method="GET">
-                    <input type="hidden" name="room_id" value="{{ $room->id }}">
-                    <button type="submit"
-                        class="rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300">予約登録</button>
-                </form>
-            </div>
-        </div>
-
-        <div>
-            @foreach ($room->reservations as $day => $reservations)
-                <div class="mt-8">
-                    <h3 class="text-xl font-bold text-gray-700">{{ $day }}</h3>
-                    <div class="grid grid-cols-3 gap-4">
-                        @foreach ($reservations as $reservation)
-                            <div class="rounded px-6 py-4 shadow-lg">
-                                <div class="mb-2 font-bold"><a
-                                        href="{{ $reservation->detailUrl }}">{{ $reservation->summary }}</a></div>
-                                <div class="mb-2 text-base text-gray-700">{{ $reservation->startAt }} ~
-                                    {{ $reservation->endAt }}</div>
-                                {{-- ViewModelでエスケープ済みなので、HTMLタグエスケープはしない --}}
-                                <p class="text-base text-gray-700">{!! $reservation->note !!} </p>
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
-            @endforeach
-        </div>
+    <div class="flex justify-between">
+        <h2 class="my-auto text-xl font-bold">{{ $room->name }}</h2>
+        <form action="/reservations/register" method="GET">
+            <input name="room_id" type="hidden" value="{{ $room->id }}">
+            <button
+                class="border-curious-blue hover:border-denim hover:bg-science-blue bg-dodger-blue rounded-md border py-1 px-3 text-white"
+                type="submit">予約登録</button>
+        </form>
     </div>
-@endsection
+
+    <div class="mt-8">
+        @foreach ($room->reservations as $day => $reservations)
+            <div class="my-8">
+                <div class="text-lg">{{ $day }}</div>
+                <hr>
+                <div class="my-4 grid grid-cols-3 gap-4">
+                    @foreach ($reservations as $reservation)
+                        <div class="bg-aqua-haze border-geyser rounded-md border p-4">
+                            <div class="mb-4 text-lg font-bold">
+                                <a class="hover:text-science-blue hover:underline"
+                                    href="{{ $reservation->detailUrl }}">{{ $reservation->summary }}</a>
+                            </div>
+                            <div>{{ $reservation->startAt }} ~ {{ $reservation->endAt }}</div>
+                            {{-- ViewModelでエスケープ済みなので、HTMLタグエスケープはしない --}}
+                            <p class="mt-4 text-sm">{!! $reservation->note !!} </p>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        @endforeach
+    @endsection
--- resources/views/rooms/reservations/register.blade.php
+++ resources/views/rooms/reservations/register.blade.php
@@ -3,94 +3,52 @@
 @section('title', '予約登録')
 
 @section('content')
-    <div>
-        @if ($errors->any())
-            <div class="mx-auto mt-4 w-3/4 bg-red-500 text-white">
-                <div class="container mx-auto flex items-center justify-start px-6 py-4">
-                    <div class="flex">
-                        <svg viewBox="0 0 40 40" class="h-6 w-6 fill-current">
-                            <path
-                                d="M20 3.36667C10.8167 3.36667 3.3667 10.8167 3.3667 20C3.3667 29.1833 10.8167 36.6333 20 36.6333C29.1834 36.6333 36.6334 29.1833 36.6334 20C36.6334 10.8167 29.1834 3.36667 20 3.36667ZM19.1334 33.3333V22.9H13.3334L21.6667 6.66667V17.1H27.25L19.1334 33.3333Z">
-                            </path>
-                        </svg>
-                    </div>
-                    <div class="ml-1">
-                        @foreach ($errors->all() as $error)
-                            {{ $error }}<br>
-                        @endforeach
-                    </div>
+    <h2 class="text-xl">予約登録</h2>
+
+    <form action="/reservations/register" method="POST">
+        @csrf
+        <input name="room_id" type="hidden" value="{{ $room_id }}">
+        <div>
+            <div class="my-3">
+                <label class="mb-2 block font-bold" for="summary">概要<span class="text-valencia">*</span></label>
+                <input
+                    class="bg-aqua-haze border-geyser focus-visible:border-science-blue w-full rounded-md border p-2 outline-none focus-visible:bg-white"
+                    id="summary" name="summary" type="text" value="{{ old('summary') }}">
+            </div>
+
+            <div class="my-3 flex">
+                <div>
+                    <label class="mb-2 block font-bold" for="start_at_date">開始日時<span class="text-valencia">*</span></label>
+                    <input
+                        class="bg-aqua-haze border-geyser focus-visible:border-science-blue rounded-md border p-2 outline-none focus-visible:bg-white"
+                        id="start_at_date" name="start_at_date" type="date" value="{{ old('start_at_date') }}">
+                    <input
+                        class="bg-aqua-haze border-geyser focus-visible:border-science-blue rounded-md border p-2 outline-none focus-visible:bg-white"
+                        id="start_at_time" name="start_at_time" type="time" value="{{ old('start_at_time') }}">
+                </div>
+                <span class="my-auto mx-4 font-bold">~</span>
+                <div>
+                    <label class="mb-2 block font-bold" for="end_at_date">終了日時<span class="text-valencia">*</span></label>
+                    <input
+                        class="bg-aqua-haze border-geyser focus-visible:border-science-blue rounded-md border p-2 outline-none focus-visible:bg-white"
+                        id="end_at_date" name="end_at_date" type="date" value="{{ old('end_at_date') }}">
+                    <input
+                        class="bg-aqua-haze border-geyser focus-visible:border-science-blue rounded-md border p-2 outline-none focus-visible:bg-white"
+                        id="end_at_time" name="end_at_time" type="time" value="{{ old('end_at_time') }}">
                 </div>
             </div>
-        @endif
 
-        @if (session('exception'))
-            <div class="mx-auto mt-4 w-3/4 bg-red-500 text-white">
-                <div class="container mx-auto flex items-center justify-start px-6 py-4">
-                    <div class="flex">
-                        <svg viewBox="0 0 40 40" class="h-6 w-6 fill-current">
-                            <path
-                                d="M20 3.36667C10.8167 3.36667 3.3667 10.8167 3.3667 20C3.3667 29.1833 10.8167 36.6333 20 36.6333C29.1834 36.6333 36.6334 29.1833 36.6334 20C36.6334 10.8167 29.1834 3.36667 20 3.36667ZM19.1334 33.3333V22.9H13.3334L21.6667 6.66667V17.1H27.25L19.1334 33.3333Z">
-                            </path>
-                        </svg>
-                    </div>
-                    <div class="ml-1">{{ session('exception') }}</div>
-                </div>
+            <div class="my-3">
+                <label class="mb-2 block font-bold" for="note">備考</label>
+                <textarea
+                    class="bg-aqua-haze border-geyser focus-visible:border-science-blue w-full rounded-md border p-2 outline-none focus-visible:bg-white"
+                    id="note" name="note" rows="4">{{ old('note') }}</textarea>
             </div>
-        @endif
-
-        <section class="mx-auto mt-4 w-3/4 rounded-md bg-white p-6 shadow-md dark:bg-gray-800">
-            <h2 class="text-lg font-semibold capitalize text-gray-700 dark:text-white">予約登録</h2>
-
-            <form action="/reservations/register" method="POST">
-                @csrf
-                <input type="hidden" name="room_id" value="{{ $room_id }}">
-                <div class="mt-4">
-                    <div>
-                        <label class="text-gray-700 dark:text-gray-200" for="summary">概要</label>
-                        <input
-                            id="summary"
-                            type="text"
-                            name="summary"
-                            value="{{ old('summary') }}"
-                            class="mt-2 block w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300">
-                    </div>
-
-                    <div class="mt-2 flex items-center">
-                        <div>
-                            <label class="block text-gray-700 dark:text-gray-200" for="start_at_date">開始日時</label>
-                            <input id="start_at_date" type="date" name="start_at_date"
-                                value="{{ old('start_at_date') }}"
-                                class="mt-2 rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300">
-                            <input id="start_at_time" type="time" name="start_at_time"
-                                value="{{ old('start_at_time') }}"
-                                class="rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300">
-                        </div>
-                        <span class="mx-4">~</span>
-                        <div>
-                            <label class="block text-gray-700 dark:text-gray-200" for="end_at_date">終了日時</label>
-                            <input id="end_at_date" type="date" name="end_at_date" value="{{ old('end_at_date') }}"
-                                class="mt-2 rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300">
-                            <input id="end_at_time" type="time" name="end_at_time" value="{{ old('end_at_time') }}"
-                                class="mt-2 rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300">
-                        </div>
-                    </div>
-
-                    <div class="mt-2">
-                        <label class="text-gray-700 dark:text-gray-200" for="note">備考</label>
-                        <textarea id="note" rows="4" name="note"
-                            class="mt-2 block w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300">{{ old('note') }}</textarea>
-                    </div>
-                </div>
-
-                <div class="mt-6 flex justify-end">
-                    <button
-                        class="transform rounded-md bg-gray-700 px-6 py-2 leading-5 text-white transition-colors duration-200 hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">登録</button>
-                </div>
-            </form>
-        </section>
-
-        <div class="mx-auto mt-5 w-3/4">
-            <a href="{{ $detail_url }}" class="text-blue-500 no-underline hover:underline">戻る</a>
         </div>
-    </div>
+
+        <div class="my-3">
+            <button
+                class="bg-dodger-blue border-curious-blue hover:border-denim hover:bg-science-blue rounded-md border py-1 px-3 text-white">登録</button>
+        </div>
+    </form>
 @endsection
