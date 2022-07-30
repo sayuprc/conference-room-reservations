fd8d0d91d71e339f41e6d505ed4d907856c29b97
sayuprc <41261915+sayuprc@users.noreply.github.com>
2022-07-30 20:46:15 +0900

    feat: Add dark mode. #6 (#65)

--- resources/views/rooms/reservations/detail.blade.php
+++ resources/views/rooms/reservations/detail.blade.php
@@ -13,7 +13,7 @@
             <div class="my-3">
                 <label class="mb-2 block font-bold" for="summary">概要<span class="text-valencia">*</span></label>
                 <input
-                    class="bg-aqua-haze border-geyser focus-visible:border-science-blue w-full rounded-md border p-2 outline-none focus-visible:bg-white"
+                    class="bg-aqua-haze border-geyser focus-visible:border-science-blue dark:border-mako dark:bg-shark-200 dark:focus-visible:border-cornflower-blue dark:focus-visible:bg-shark-300 w-full rounded-md border p-2 outline-none focus-visible:bg-white"
                     id="summary" name="summary" type="text" value="{{ $reservation->summary }}">
             </div>
 
@@ -21,20 +21,20 @@
                 <div>
                     <label class="mb-2 block font-bold" for="start_at_date">開始日時<span class="text-valencia">*</span></label>
                     <input
-                        class="bg-aqua-haze border-geyser focus-visible:border-science-blue rounded-md border p-2 outline-none focus-visible:bg-white"
+                        class="bg-aqua-haze border-geyser focus-visible:border-science-blue dark:bg-shark-200 dark:border-mako dark:focus-visible:border-cornflower-blue dark:focus-visible:bg-shark-300 rounded-md border p-2 outline-none focus-visible:bg-white"
                         id="start_at_date" name="start_at_date" type="date" value="{{ $reservation->startAtDate }}">
                     <input
-                        class="bg-aqua-haze border-geyser focus-visible:border-science-blue rounded-md border p-2 outline-none focus-visible:bg-white"
+                        class="bg-aqua-haze border-geyser focus-visible:border-science-blue dark:bg-shark-200 dark:border-mako dark:focus-visible:border-cornflower-blue dark:focus-visible:bg-shark-300 rounded-md border p-2 outline-none focus-visible:bg-white"
                         id="start_at_time" name="start_at_time" type="time" value="{{ $reservation->startAtTime }}">
                 </div>
                 <span class="my-auto mx-4 font-bold">~</span>
                 <div>
                     <label class="mb-2 block font-bold" for="end_at_date">終了日時<span class="text-valencia">*</span></label>
                     <input
-                        class="bg-aqua-haze border-geyser focus-visible:border-science-blue rounded-md border p-2 outline-none focus-visible:bg-white"
+                        class="bg-aqua-haze border-geyser focus-visible:border-science-blue dark:bg-shark-200 dark:border-mako dark:focus-visible:border-cornflower-blue dark:focus-visible:bg-shark-300 rounded-md border p-2 outline-none focus-visible:bg-white"
                         id="end_at_date" name="end_at_date" type="date" value="{{ $reservation->endAtDate }}">
                     <input
-                        class="bg-aqua-haze border-geyser focus-visible:border-science-blue rounded-md border p-2 outline-none focus-visible:bg-white"
+                        class="bg-aqua-haze border-geyser focus-visible:border-science-blue dark:bg-shark-200 dark:border-mako dark:focus-visible:border-cornflower-blue dark:focus-visible:bg-shark-300 rounded-md border p-2 outline-none focus-visible:bg-white"
                         id="end_at_time" name="end_at_time" type="time" value="{{ $reservation->endAtTime }}">
                 </div>
             </div>
@@ -42,16 +42,16 @@
             <div class="my-3">
                 <label class="mb-2 block font-bold" for="note">備考</label>
                 <textarea
-                    class="bg-aqua-haze border-geyser focus-visible:border-science-blue w-full rounded-md border p-2 outline-none focus-visible:bg-white"
+                    class="bg-aqua-haze border-geyser focus-visible:border-science-blue dark:border-mako dark:bg-shark-200 dark:focus-visible:border-cornflower-blue dark:focus-visible:bg-shark-300 w-full rounded-md border p-2 outline-none focus-visible:bg-white"
                     id="note" name="note" rows="4">{{ $reservation->note }}</textarea>
             </div>
         </div>
 
         <div class="my-3 flex justify-between">
             <button
-                class="bg-dodger-blue border-curious-blue hover:border-denim hover:bg-science-blue rounded-md border py-1 px-3 text-white">更新</button>
+                class="bg-dodger-blue border-curious-blue hover:border-denim hover:bg-science-blue dark:border-goblin-100 dark:bg-goblin-200 dark:hover:bg-fruit-salad rounded-md border py-1 px-3 text-white">更新</button>
             <button
-                class="hover:border-tamarillo hover:bg-shiraz text-valencia border-iron bg-aqua-haze rounded-md border py-1 px-3 hover:text-white"
+                class="hover:border-tamarillo hover:bg-shiraz text-valencia border-iron bg-aqua-haze dark:text-mandy dark:bg-bright-gray dark:border-river-bed-100 dark:hover:text-botticelli dark:hover:bg-flush-mahogany dark:hover:border-mandy rounded-md border py-1 px-3 hover:text-white"
                 id="reservation-delete-button">削除</button>
         </div>
     </form>
