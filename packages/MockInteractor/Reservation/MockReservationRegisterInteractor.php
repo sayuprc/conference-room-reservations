06ead86997245ba8758bb5d4760bc3a6efe62509
sayuprc <41261915+sayuprc@users.noreply.github.com>
2022-08-13 18:43:18 +0900

    refactor: Set up mock classes. (#109)

--- packages/MockInteractor/Reservation/MockReservationRegisterInteractor.php
+++ packages/MockInteractor/Reservation/MockReservationRegisterInteractor.php
@@ -17,7 +17,8 @@ use packages\Domain\Domain\Reservation\StartAt;
 use packages\Domain\Domain\Reservation\Summary;
 use packages\Domain\Domain\Room\Exception\NotFoundException;
 use packages\Domain\Domain\Room\RoomId;
-use packages\Domain\Domain\Room\RoomService;
+use packages\Domain\Domain\Room\RoomRepositoryInterface;
+use packages\Domain\Domain\Slack\SlackAPIRepositoryInterface;
 use packages\UseCase\Reservation\Common\ReservationModel;
 use packages\UseCase\Reservation\Register\ReservationRegisterRequest;
 use packages\UseCase\Reservation\Register\ReservationRegisterResponse;
@@ -26,9 +27,14 @@ use packages\UseCase\Reservation\Register\ReservationRegisterUseCaseInterface;
 class MockReservationRegisterInteractor implements ReservationRegisterUseCaseInterface
 {
     /**
-     * @var ReservationRepositoryInterface $repository
+     * @var RoomRepositoryInterface $roomRepository
      */
-    private ReservationRepositoryInterface $repository;
+    private RoomRepositoryInterface $roomRepository;
+
+    /**
+     * @var ReservationRepositoryInterface $reservationRepository
+     */
+    private ReservationRepositoryInterface $reservationRepository;
 
     /**
      * @var ReservationService $service
@@ -36,27 +42,28 @@ class MockReservationRegisterInteractor implements ReservationRegisterUseCaseInt
     private ReservationService $service;
 
     /**
-     * @var RoomService $roomService
+     * @var SlackAPIRepositoryInterface $slackAPIRepository
      */
-    private RoomService $roomService;
+    private SlackAPIRepositoryInterface $slackAPIRepository;
 
     /**
-     * @param ReservationRepositoryInterface $repository
+     * @param RoomRepositoryInterface        $roomRepository
+     * @param ReservationRepositoryInterface $reservationRepository
      * @param ReservationService             $service
-     * @param RoomService                    $roomService
+     * @param SlackAPIRepositoryInterface    $slackAPIRepository
      *
      * @return void
      */
     public function __construct(
-        ReservationRepositoryInterface $repository,
+        RoomRepositoryInterface $roomRepository,
+        ReservationRepositoryInterface $reservationRepository,
         ReservationService $service,
-        RoomService $roomService
+        SlackAPIRepositoryInterface $slackAPIRepository
     ) {
-        $this->repository = $repository;
-
+        $this->roomRepository = $roomRepository;
+        $this->reservationRepository = $reservationRepository;
         $this->service = $service;
-
-        $this->roomService = $roomService;
+        $this->slackAPIRepository = $slackAPIRepository;
     }
 
     /**
@@ -80,7 +87,9 @@ class MockReservationRegisterInteractor implements ReservationRegisterUseCaseInt
             new Note($request->getNote())
         );
 
-        if (! $this->roomService->exists($newReservation->getRoomId())) {
+        $room = $this->roomRepository->find($newReservation->getRoomId());
+
+        if ($room === null) {
             throw new NotFoundException('ID: ' . $newReservation->getRoomId()->getValue() . ' is not found.');
         }
 
@@ -88,7 +97,7 @@ class MockReservationRegisterInteractor implements ReservationRegisterUseCaseInt
             throw new PeriodicDuplicationException('There is a reservation for a specified period of time.');
         }
 
-        $this->repository->insert($newReservation);
+        $this->reservationRepository->insert($newReservation);
 
         $reservationModel = new ReservationModel(
             $newReservation->getRoomId()->getValue(),
@@ -97,6 +106,24 @@ class MockReservationRegisterInteractor implements ReservationRegisterUseCaseInt
             $newReservation->getStartAt()->getValue(),
             $newReservation->getEndAt()->getValue(),
             $newReservation->getNote()->getValue()
+        );
+
+        $showLimit = 25;
+
+        $this->slackAPIRepository->postMessage(
+            sprintf(
+                "予約が登録されました。\n```会議室名: %s\n概要: %s\n日時: %s ~ %s\n備考: %s```\n%s/reservations/show/%s",
+                $room->getRoomName()->getValue(),
+                $reservationModel->summary,
+                $reservationModel->startAt->format('Y/m/d H:i'),
+                $reservationModel->endAt->format('Y/m/d H:i'),
+                // 25文字以上は...にする
+                $showLimit < mb_strlen($reservationModel->note)
+                    ? mb_substr($reservationModel->note, 0, $showLimit) . '...'
+                    : $reservationModel->note,
+                config('app.url'),
+                $reservationModel->reservationId
+            )
         );
 
         return new ReservationRegisterResponse($reservationModel);
