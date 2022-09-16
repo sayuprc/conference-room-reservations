dfb4d900216aac17e3dd301319d28232d1d4f6ec
sayuprc <41261915+sayuprc@users.noreply.github.com>
2022-09-16 23:18:46 +0900

    feat: Created a domain model for the reservation template. (#129)
    * feat: Created a domain model for the reservation template.
    
    * Fixed phpdoc.

---
+++ packages/Infrastructure/ReservationTemplate/ReservationTemplateRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace packages\Infrastructure\ReservationTemplate;
+
+use packages\Domain\Domain\ReservationTemplate\ReservationTemplateRepositoryInterface;
+
+class ReservationTemplateRepository implements ReservationTemplateRepositoryInterface
+{
+}
