2dc4a429e31a96c66b14eafc80c9b6125467299f
sayuprc <41261915+sayuprc@users.noreply.github.com>
2022-09-28 21:52:52 +0900

    :white_check_mark: Change parent class. #146 (#147)

--- tests/Feature/Reservation/DeleteReservationTest.php
+++ tests/Feature/Reservation/DeleteReservationTest.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace Tests\Feature\Reservation;
 
 use App\Models\Room;
-use Tests\TestCase;
+use Tests\Feature\FeatureTestCase;
 
-class DeleteReservationTest extends TestCase
+class DeleteReservationTest extends FeatureTestCase
 {
     /**
      * テスト用会議室取得
